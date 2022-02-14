0a87c727c78fb03adf3f83214d2beeb71932b68b
Jemimah Sai <jemimah.beryl.sai@gmail.com>
2022-02-14 15:59:18 +0800

    Add action buttons
    - include functionality for saving, retrieving and clearing components

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const noResultsFoundContainer = document.getElementById('no_results_found_contai
 const componentSearchListContainer = document.getElementById('component_search_list_container')
 const canvasAreaContainer = document.getElementById('canvas_area_container');
 const searchBarIcon = document.getElementById('search_bar_icon');
+const saveProgressButton = document.getElementById("save_progress");
+const clearCanvasButton = document.getElementById("clear_canvas");
 
 /** INITIALIZERS */
 let isSearching = false;
@@ -339,8 +341,32 @@ toggleComponentTabButton.addEventListener('click', e => {
     }
 });
 
+saveProgressButton.addEventListener('click', e => {
+    if (confirm("Are you sure you want to save your progress?")) {
+        localStorage.setItem("blackbox_progress", JSON.stringify(droppedComponents, null, 2)); // Save to localstorage
+    }
+});
+
+clearCanvasButton.addEventListener('click', e => {
+    if (confirm("Are you sure you want to clear canvas?")) {
+        localStorage.removeItem("blackbox_progress"); // Remove from localstorage
+
+        droppedComponents = [];
+
+        canvasAreaContainer.innerHTML = `<div class="target-component-container" ondrop="dropToCanvas(event)" ondragover="allowDropSource(event)">
+                                            <div role="target" class="target-component-empty">
+                                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="plus" class="svg-inline--fa fa-plus fa-w-14 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                                <path fill="currentColor" d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path>
+                                                </svg>
+                                            </div>
+                                        </div>`;
+    }
+});
+
 /** PAGE LOAD INITIALIZATION */
 document.addEventListener("DOMContentLoaded", function() {
+    const currentProgress = JSON.parse(localStorage.getItem("blackbox_progress")); // get saved progress from localstorage
+
     componentsToggableTabContainer.innerHTML = `<div class="components-toggable-tab" id="components_toggable_tab">
                                                     ${ Object.keys(componentsList).map(list => `<div class="components-toggable-tab" id="components_toggable_tab">
                                                         <div class="component-header" id="toggle_component_header_${list}">
@@ -360,11 +386,42 @@ document.addEventListener("DOMContentLoaded", function() {
                                                             </div>
                                                         </div>`).join("\n") }
                                                 </div>`;
-    canvasAreaContainer.innerHTML = `<div class="target-component-container" ondrop="dropToCanvas(event)" ondragover="allowDropSource(event)">
-                                        <div role="target" class="target-component-empty">
-                                            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="plus" class="svg-inline--fa fa-plus fa-w-14 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                                            <path fill="currentColor" d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path>
-                                            </svg>
-                                        </div>
-                                    </div>`;
+    if (currentProgress && currentProgress.length > 0) {
+        droppedComponents = [...droppedComponents, ...currentProgress];
+
+        canvasAreaContainer.innerHTML = `${droppedComponents.map((component, index) => {
+            const spaceBetweenComponents = index * 150;
+            const arrowPositionBetweenComponents = index > 1 ? index * 80 + (index * 70 - 70) : index * 80;
+
+            return `<div class="target-component-container with-component" id="${component._id}" style="top: calc(15% + ${spaceBetweenComponents}px);" ondragstart="dragDroppedComponent(event)" ondragover="allowDropSource(event)">
+                        <div role="target" class="target-component" title="${component.type}" draggable="true">
+                            ${component.logoName}
+                            <span class="label">${component.name}</span>
+                        </div>
+                        ${component.properties.connectedAfter ? '' : `<div class="whitespace"></div>`}
+                    </div>
+                    ${index > 0 ? `<div class="target-component-arrow-down" style="top: calc(14.8% + ${arrowPositionBetweenComponents}px);">
+                                                            <div class="line"></div>
+                                                            <div class="triangle"></div>
+                                                        </div>` : ''}
+                    `}).join("\n") + 
+                    (droppedComponents[droppedComponents.length - 1].properties.connectedAfter ? 
+                        `<div class="target-component-container" style="top: calc(15% + ${droppedComponents.length * 150}px" ondrop="dropToCanvas(event)" ondragover="allowDropSource(event)">
+                            <div role="target" class="target-component-empty">
+                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="plus" class="svg-inline--fa fa-plus fa-w-14 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                <path fill="currentColor" d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path>
+                                </svg>
+                            </div>
+                            <div class="whitespace"></div>
+                        </div>` : '') }`;
+    } else {
+        canvasAreaContainer.innerHTML = `<div class="target-component-container" ondrop="dropToCanvas(event)" ondragover="allowDropSource(event)">
+            <div role="target" class="target-component-empty">
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="plus" class="svg-inline--fa fa-plus fa-w-14 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                <path fill="currentColor" d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path>
+                </svg>
+            </div>
+        </div>`;
+    }
+
 });
